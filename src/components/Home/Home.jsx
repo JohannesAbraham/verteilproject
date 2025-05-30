b1aa67793ccb37d4a9dd19185129a8aaf2a55a8e
HannahVarghese2805 <hannahvarghese2805@gmail.com>
2025-05-30 09:48:02 +0530

    sixth commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -27,14 +27,40 @@ function Home() {
     }
   ];
 
+  const airlineNewsItems = [
+    {
+      id: 1,
+      image: 'https://media.istockphoto.com/id/155380716/photo/commercial-jet-flying-over-clouds.jpg?s=612x612&w=0&k=20&c=idhnJ7ZdrLA1Dv5GO2R28A8WCx1SXCFVLu5_2cfdvXw=',
+      heading: '✈️ New Commercial Jet Over the Clouds!',
+      date: 'May 30, 2025'
+    },
+    {
+      id: 2,
+      image: 'https://media.istockphoto.com/id/155380716/photo/commercial-jet-flying-over-clouds.jpg?s=612x612&w=0&k=20&c=idhnJ7ZdrLA1Dv5GO2R28A8WCx1SXCFVLu5_2cfdvXw=',
+      heading: '🛫 Airline Industry Sees Recovery',
+      date: 'May 29, 2025'
+    },
+    {
+      id: 3,
+      image: 'https://media.istockphoto.com/id/155380716/photo/commercial-jet-flying-over-clouds.jpg?s=612x612&w=0&k=20&c=idhnJ7ZdrLA1Dv5GO2R28A8WCx1SXCFVLu5_2cfdvXw=.',
+      heading: '📰 Airline Tech Innovations',
+      date: 'May 28, 2025'
+    }
+  ];
+
   const [newsOrder, setNewsOrder] = useState(newsItems);
+  const [airlineNewsOrder, setAirlineNewsOrder] = useState(airlineNewsItems);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setNewsOrder((prevOrder) => {
-        const newOrder = [...prevOrder];
-        const firstItem = newOrder.shift();
-        newOrder.push(firstItem);
+      setNewsOrder(prev => {
+        const newOrder = [...prev];
+        newOrder.push(newOrder.shift());
+        return newOrder;
+      });
+      setAirlineNewsOrder(prev => {
+        const newOrder = [...prev];
+        newOrder.push(newOrder.shift());
         return newOrder;
       });
     }, 7000);
@@ -48,51 +74,89 @@ function Home() {
           <img src={verteilLogo} alt="Verteil Logo" className="company-icon" />
         </div>
 
-        <nav className={`nav-menu ${menuOpen ? 'open' : ''}`}>
-          <a href="#home" onClick={() => setMenuOpen(false)}>Home</a>
-          <a href="#about" onClick={() => setMenuOpen(false)}>About</a>
-          <a href="#services" onClick={() => setMenuOpen(false)}>Services</a>
-          <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
-        </nav>
-
+         
         <div className="profile-menu">
           <img src={profileLogo} alt="Profile Icon" className="profile-logo" />
         </div>
       </header>
 
       <main className="main-content">
-        <h2>Welcome to Verteil Home Page!</h2>
+        
+        <div className="news-sections">
+          <section className="company-news-box">
+            <h3>Company News</h3>
+            <div className="news-layout">
+              <div className="news-item large">
+                <img src={newsOrder[0].image} alt="News" />
+                <div className="news-text">
+                  <h4>{newsOrder[0].heading}</h4>
+                  <p className="news-date">{newsOrder[0].date}</p>
+                </div>
+              </div>
+              <div className="news-right">
+                <div className="news-item medium">
+                  <img src={newsOrder[1].image} alt="News" />
+                  <div className="news-text">
+                    <h4>{newsOrder[1].heading}</h4>
+                    <p className="news-date">{newsOrder[1].date}</p>
+                  </div>
+                </div>
+                <div className="news-item small">
+                  <img src={newsOrder[2].image} alt="News" />
+                  <div className="news-text">
+                    <h4>{newsOrder[2].heading}</h4>
+                    <p className="news-date">{newsOrder[2].date}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
 
-        <section className="company-news-box">
-          <h3>Company News</h3>
-          <div className="news-layout">
-            <div className="news-item large">
-              <img src={newsOrder[0].image} alt="News" />
-              <div className="news-text">
-                <h4>{newsOrder[0].heading}</h4>
-                <p className="news-date">{newsOrder[0].date}</p>
-              </div>
-            </div>
-            <div className="news-right">
-              <div className="news-item medium">
-                <img src={newsOrder[1].image} alt="News" />
-                <div className="news-text">
-                  <h4>{newsOrder[1].heading}</h4>
-                  <p className="news-date">{newsOrder[1].date}</p>
-                </div>
-              </div>
-              <div className="news-item small">
-                <img src={newsOrder[2].image} alt="News" />
-                <div className="news-text">
-                  <h4>{newsOrder[2].heading}</h4>
-                  <p className="news-date">{newsOrder[2].date}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+         <section className="airline-news-box">
+  <h3>Airline News</h3>
+  <div className="news-layout">
+    <div className="news-item large">
+      <img
+        src="https://media.istockphoto.com/id/155380716/photo/commercial-jet-flying-over-clouds.jpg?s=612x612&w=0&k=20&c=idhnJ7ZdrLA1Dv5GO2R28A8WCx1SXCFVLu5_2cfdvXw="
+        alt="Airline News"
+      />
+      <div className="news-text">
+        <h4>✈️ New International Routes Announced</h4>
+        <p className="news-date">May 29, 2025</p>
+      </div>
     </div>
+    <div className="news-right">
+      <div className="news-item medium">
+        <img
+          src="https://media.istockphoto.com/id/155380716/photo/commercial-jet-flying-over-clouds.jpg?s=612x612&w=0&k=20&c=idhnJ7ZdrLA1Dv5GO2R28A8WCx1SXCFVLu5_2cfdvXw="
+          alt="Airline News"
+        />
+        <div className="news-text">
+          <h4>🛫 Safety Protocol Updates for Summer</h4>
+          <p className="news-date">May 28, 2025</p>
+        </div>
+      </div>
+      <div className="news-item small">
+        <img
+          src="https://media.istockphoto.com/id/155380716/photo/commercial-jet-flying-over-clouds.jpg?s=612x612&w=0&k=20&c=idhnJ7ZdrLA1Dv5GO2R28A8WCx1SXCFVLu5_2cfdvXw="
+          alt="Airline News"
+        />
+        <div className="news-text">
+          <h4>💺 New In-Flight Entertainment Systems</h4>
+          <p className="news-date">May 27, 2025</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+        </div>
+      </main>
+      <footer className="footer">
+        <p>© {new Date().getFullYear()} Verteil Technologies. All rights reserved.</p>
+      </footer>
+    </div>
+    
   );
 }
 
